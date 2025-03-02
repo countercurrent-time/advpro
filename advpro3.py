8134ab9553e0187f23d9f7d122e91706d2a1e86d
countercurrent-time <358935062@qq.com>
2025-03-02 21:35:39 +0800

    set token limit to 2000

--- advpro3.py
+++ advpro3.py
@@ -479,11 +479,11 @@ class AdvPro:
             
             inputs = tokenizer(current_prompt, return_tensors="pt").to(self.device)
             
-            # 对于输入上限为2047个 token 的 CodeGen2-1B，检查 token 数量是否超过 2047
-            if inputs["input_ids"].shape[1] > 2047:
-                inputs["input_ids"] = inputs["input_ids"][:, -2047:]
+            # 对于输入上限为2000个 token 的 CodeGen2-1B，检查 token 数量是否超过 2000
+            if inputs["input_ids"].shape[1] > 2000:
+                inputs["input_ids"] = inputs["input_ids"][:, -2000:]
                 if "attention_mask" in inputs:
-                    inputs["attention_mask"] = inputs["attention_mask"][:, -2047:]
+                    inputs["attention_mask"] = inputs["attention_mask"][:, -2000:]
             inputs = inputs.to(self.device)
             
             # with torch.no_grad():
